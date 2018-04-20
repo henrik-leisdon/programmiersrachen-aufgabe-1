7b11aac40ff8d4bb7fc466be09bbe2f3324fc16b
thetrippleh <henrik.leisdon@uni-weimar.de>
2018-04-20 15:50:54 +0200

    finished 1.10 - working

---
+++ build/source/CMakeFiles/aufgabe1_10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe1_10.dir/aufgabe1_10.cpp.o"
+  "aufgabe1_10.pdb"
+  "aufgabe1_10"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe1_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/source/CMakeFiles/aufgabe1_9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe1_9.dir/aufgabe1_9.cpp.o"
+  "aufgabe1_9.pdb"
+  "aufgabe1_9"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe1_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
